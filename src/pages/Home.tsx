ff0113c98268a73f8e8a28e52fcf756c6a394bc7
Diogo Izele <diogo.izele@cwi.com.br>
2022-10-17 17:06:43 -0300

    feat(home): creates handle tasks functions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks((oldTasks) => [
+      ...oldTasks,
+      {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const task = tasks.find((task) => task.id === id);
+
+    if (task) {
+      const newTask: Task = { ...task, done: !task.done };
+
+      setTasks((oldTasks) =>
+        oldTasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+
+          return task;
+        })
+      );
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks((oldTasks) => oldTasks.filter((task) => task.id !== id));
   }
 
   return (
@@ -26,18 +47,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
